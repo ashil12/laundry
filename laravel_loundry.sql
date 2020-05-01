b22b1791d86d661b32610f19fdabae89498e60cc
ashil12 <moch.ashil00@gmail.com>
2020-05-01 21:39:30 +0700

    Initial commit

---
+++ laravel_loundry.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Mar 2020 pada 03.19
+-- Versi server: 10.1.39-MariaDB
+-- Versi PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel_loundry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_detail_transaksi`
+--
+
+CREATE TABLE `tb_detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `transaksi_id` varchar(200) NOT NULL,
+  `paket_id` varchar(200) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `sub_total` varchar(200) NOT NULL,
+  `harga_total` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jenis`
+--
+
+CREATE TABLE `tb_jenis` (
+  `id_jenis` int(11) NOT NULL,
+  `nama_jenis` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_member`
+--
+
+CREATE TABLE `tb_member` (
+  `id_member` int(11) NOT NULL,
+  `nama_member` varchar(200) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `tlp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_outlet`
+--
+
+CREATE TABLE `tb_outlet` (
+  `id_outlet` int(11) NOT NULL,
+  `nama_outlet` varchar(200) NOT NULL,
+  `alamat_outlet` text NOT NULL,
+  `tlp_outlet` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_paket`
+--
+
+CREATE TABLE `tb_paket` (
+  `id_paket` int(11) NOT NULL,
+  `outlet_paket_id` varchar(200) NOT NULL,
+  `jenis_id` varchar(200) NOT NULL,
+  `nama_paket` varchar(200) NOT NULL,
+  `harga_paket` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `transaksi_outlet_id` varchar(200) NOT NULL,
+  `kode_invoice` varchar(200) NOT NULL,
+  `member_id` varchar(200) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `tgl_bayar` date NOT NULL,
+  `biaya_tambahan` varchar(200) NOT NULL,
+  `diskon` double NOT NULL,
+  `pajak` int(11) NOT NULL,
+  `status_pakaian` enum('baru','proses','selesai','diambil') NOT NULL,
+  `status_bayar` enum('dibayar','belum_dibayar','','') NOT NULL,
+  `user_id` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` enum('A','K','O','') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@gmail.com', NULL, '$2y$10$e8sCGsY0B1pf1j2ex4HdxOjzxEz0P34lyWLSFnB6BlfyDarli94NW', 'A', NULL, '2020-03-24 08:57:10', '2020-03-25 01:40:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`);
+
+--
+-- Indeks untuk tabel `tb_jenis`
+--
+ALTER TABLE `tb_jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indeks untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indeks untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  ADD PRIMARY KEY (`id_outlet`);
+
+--
+-- Indeks untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indeks untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_jenis`
+--
+ALTER TABLE `tb_jenis`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
